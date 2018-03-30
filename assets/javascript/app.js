bfed0a24b388233fdfa9e828d47d5aba1c27b8d2
Dorian Macias <33275910+dorianinc@users.noreply.github.com>
2018-03-30 07:51:31 -0700

    Add files via upload
    Completed all the JavaScript
    - added timer, made sure everything runs thru properly
    Cleaned up CSS and JavaScript
    -Added comments to help read code

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,18 @@
+
 // Global Variables //
-var timer = 30;
-var currentQuestion;
-var currentQuestionNumber = 0;
+/*-------------------------------------------------------------------*/
+var time = 30;
+var timer;
+
 var correct = 0;
 var inCorrect = 0;
 var unAnswered = 0;
 
+var currentQuestion;
+var currentQuestionNumber = 0;
+
+// Object containing questions //
+/*-------------------------------------------------------------------*/
 var questions =
 [
     {
@@ -44,7 +51,7 @@ var questions =
         wrongImage: ["<img class = 'center-block movie' src='assets/images/wrongAnswer5.gif'>"]
     },
 ];
-
+/*-------------------------------------------------------------------*/
 /* OnLoad, create Start button */
 
 $(document).ready(function() {
@@ -56,84 +63,125 @@ $(document).ready(function() {
     
     startButton();
 });
-
+/*-------------------------------------------------------------------*/
 /* OnClick Functions */
 
-$('body').on('click', '.start-button', function()
+$('body').on('click', '.start-button', function() // On Click, starts game and timer
 {
-   startGame(); 
+    startGame();
+    countDown(); 
+});
+
+$('body').on('click', '.reset-button', function() // On Click, restarts game and timer
+{
+   reset();
+   countDown(); 
     // Start Timer
 });
 
-$('body').on('click', '.reset-button', function()
-{
-   reset(); 
-    // Start Timer
-});
-
-$("body").on("click", ".options", function()
+$("body").on("click", ".options", function() // On Click, takes user selected option and runs it thru the CheckWinLose function
 {
     selectedOption = $(this).text();
-        checkWinLose();
+    checkWinLose();
 });
 
-
+/*-------------------------------------------------------------------*/
 /* Start Game Function, boots up gameHTML containing questions and options*/
-function startGame()
+
+function startGame() // Starts game, boots up questions and GameHTML
 {
     currentQuestion = questions[currentQuestionNumber];
-    gameHTML = "<span class='glyphicon glyphicon-time timer'>"+ " Remaining:30" + "</span>" + "<p class = 'text-center question'> " + currentQuestion.text[0] + "</br>" + currentQuestion.text[1] + "</p>" + "<p class = 'text-center options'>" + "A: " + currentQuestion.options[0] + "</p>" + "<p class = 'text-center options'>" + "B: " + currentQuestion.options[1] + "</p>" + "<p class = 'text-center options'>" + "C: " + currentQuestion.options[2] + "</p>" + "<p class = 'text-center options'>" + "D: " + currentQuestion.options[3] + "</p>";
+    gameHTML ="<p class = 'timer'>" + "Time Remaining: " + time + "</p>" + "<p class = 'text-center question'> " + currentQuestion.text[0] + "</br>" + currentQuestion.text[1] + "</p>" + "<p class = 'text-center options'>" + "A: " + currentQuestion.options[0] + "</p>" + "<p class = 'text-center options'>" + "B: " + currentQuestion.options[1] + "</p>" + "<p class = 'text-center options'>" + "C: " + currentQuestion.options[2] + "</p>" + "<p class = 'text-center options'>" + "D: " + currentQuestion.options[3] + "</p>";
     $('.mainContent').html(gameHTML);
+    (countDown);
 }
 
+function countDown() // Function in charge of 30 second countdown
+{
+    timer = setInterval(thirtySeconds, 1000)
+    function thirtySeconds()
+    {
+        if(time === 0)
+        {
+            timeOut();
+            clearInterval(timer);
+        }
+        if(time > 0)
+        {   
+            time--;
+            console.log(time);
+        }
+        $(".timer").html("Time Remaining: " + time);
+    }
+}
 
-/* Check for if selected option is correct or not */
+/*-------------------------------------------------------------------*/
+/* If option is Selected run checkWinorLose, If timer hits 0 run timeout function */
+
 function checkWinLose()
 {
-    if(selectedOption === currentQuestion.answer)
+    if(selectedOption === currentQuestion.answer) // If user selected option is correct 
     {
         winImage = currentQuestion.rightImage;
         gameHTML = "<p class = 'text-center options'>" + "Thats Correct!" + "</p>" + winImage;
         $('.mainContent').html(gameHTML);
         correct++;
         currentQuestionNumber++;
+        clearInterval(timer);
         setTimeout(next, 5000);
     }
-    else if(selectedOption !== currentQuestion.answer)
+    else if(selectedOption !== currentQuestion.answer) // If user selected option is incorrect
     {
         loseImage = currentQuestion.wrongImage;
         gameHTML = "<p class = 'text-center options'>" + "Thats Incorrect! The correct answer was " + currentQuestion.answer + "</p>" + loseImage;
         $('.mainContent').html(gameHTML);
         inCorrect++;
         currentQuestionNumber++;
+        clearInterval(timer);;
         setTimeout(next, 5000);
-    }    
-        console.log ("current number is: " + currentQuestionNumber);
+    }
+    console.log ("current number is: " + currentQuestionNumber);
 }
 
+function timeOut()   // If timer hits 0
+{
+    loseImage = currentQuestion.wrongImage;
+    gameHTML = "<p class = 'text-center timesUp'> " + "Aw, looks like you timed out! The correct answer was " + currentQuestion.answer + "</p>" + loseImage; 
+    $('.mainContent').html(gameHTML);
+    unAnswered++;
+    currentQuestionNumber++;
+    setTimeout(next, 4000);  //  change to 4000 or other amount
+    console.log ("current number is: " + currentQuestionNumber);
+}
 
+/*-------------------------------------------------------------------*/
 /* Function incharge of moving onto the next question and final tally page */
+
 function next()
 {
-    if(currentQuestionNumber < 5)
+    if(currentQuestionNumber < 5) // Enables us to loop thru all the questions
     {
         startGame();
+        time = 30;
+        countDown();
     }
-    else
+    else // If all questions have been looped thru this sends up to the finally tally page
     {
         finalTally();
     }
 }
-
+/*-------------------------------------------------------------------*/
+/* Sends you to end of page to Tally Score*/
 function finalTally()
 {
     gameHTML = "<p class = 'text-center closer'> " + "Now lets see how you did..." + "</p>" + "<p class = 'text-center results'>" + "Correct: " + correct + "</p>" + "<p class = 'text-center results'>" + "Incorrect: " + inCorrect + "</p>" + "<p class = 'text-center results'>" + "Unanswered: " + unAnswered + "</p>"  + "<p class='button'><a class='btn btn-danger btn-lg btn-block reset-button ' href='#' role='button'>Encore?</a></p>";
     $('.mainContent').html(gameHTML);
 }
-
+/*-------------------------------------------------------------------*/
+/* Reset Function*/
 function reset()
 {
-    timer = 30;
+    time = 30;
     currentQuestionNumber = 0;
     correct = 0;
     inCorrect = 0;
